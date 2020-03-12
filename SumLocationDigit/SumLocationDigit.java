e4583e48977809371c77e9e8ccccbe50a979e8b8
KimMinHo <rkawk6633@gmail.com>
2020-03-12 23:36:47 +0900

    자릿수 더하기

---
+++ SumLocationDigit/SumLocationDigit.java
@@ -0,0 +1,13 @@
+
+public class SumLocationDigit {
+	public int solution(int n) {
+        int sum = 0;
+        while(n > 0){
+            sum += n%10;
+            n /=10;
+        }
+
+
+        return sum;
+    }
+}
