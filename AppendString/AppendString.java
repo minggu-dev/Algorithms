007e3f1e6bd78517b93801b094c9b1bd2ef5f8af
KimMinHo <rkawk6633@gmail.com>
2020-04-01 21:44:05 +0900

    특정 문자열 이어붙이기 ver2

--- AppendString/AppendString.java
+++ AppendString/AppendString.java
@@ -4,11 +4,24 @@ public class AppendString {
 	 * @param n
 	 * @return
 	 */
+	/*3항연산자 사용
 	public String solution(int n) {
 		StringBuilder builder = new StringBuilder();
 		for (int i = 0; i < n; i++) {
 				builder.append(i % 2 == 0 ? "수" : "박");
 		}
+		return builder.toString();
+	}
+	*/
+	//2개씩 이어 붙이기
+	public String solution(int n) {
+		StringBuilder builder = new StringBuilder();
+		for (int i = 0; i < n/2; i++) {
+				builder.append("수박");
+		}
+        if(n % 2 == 1){
+            builder.append("수");
+        }
 		return builder.toString();
 	}
 }
